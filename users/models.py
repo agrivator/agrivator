5b6cb6907b84e8698ffc450cefcb7d57bb176525
Kiran Johns <kiranjohns369@gmail.com>
2020-03-25 14:50:34 +0530

    user model

--- users/models.py
+++ users/models.py
@@ -1,12 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.urls import reverse
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class User(AbstractUser):
-    USER_TYPE = [
-        (0,"Admin"),
-        (1,"Farmer"),
-    ]
-    user_type = models.IntegerField(choices=USER_TYPE,blank=False),
+from .managers import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True, primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     age = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(100)])
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
