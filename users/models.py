99624fd17eafa54aff4bbc14e59c96806de0c218
Kiran Johns <kiranjohns369@gmail.com>
2020-03-25 13:31:26 +0530

    abstract user init

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPE = [
+        (0,"Admin"),
+        (1,"Farmer"),
+    ]
+    user_type = models.IntegerField(choices=USER_TYPE,blank=False),
+    age = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(100)])
