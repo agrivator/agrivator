24c60dbb746f400e765ee308a345bb53d54e3f6d
Kiran Johns <kiranjohns369@gmail.com>
2020-04-08 10:56:58 +0530

    Tesla Model inu indo item classes

--- users/models.py
+++ users/models.py
@@ -5,6 +5,12 @@ from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from .managers import CustomUserManager
 
+phone_number_regex = RegexValidator(
+        regex="^((\+91|91|0)[\- ]{0,1})?[456789]\d{9}$",
+        message="Please Enter 10/11 digit mobile number or landline as 0<std code><phone number>",
+        code="invalid_mobile",
+    )
+
 """
 Currently Only super user gets to view admin dashboard
 Staff Users can login to admin but no permission to view anything
@@ -15,15 +21,6 @@ Non Staff users are the end users the farmers
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True, primary_key=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    age = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
-    phone_number_regex = RegexValidator(
-        regex="^((\+91|91|0)[\- ]{0,1})?[456789]\d{9}$",
-        message="Please Enter 10/11 digit mobile number or landline as 0<std code><phone number>",
-        code="invalid_mobile",
-    )
-    phone = models.CharField(max_length=14, validators=[phone_number_regex])
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
@@ -36,4 +33,26 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
+class Farmer(User):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    phone_one = models.CharField(max_length=14, validators=[phone_number_regex])
+    phone_two = models.CharField(max_length=14, validators=[phone_number_regex],null=True, blank=True)
+    address = models.TextField(max_length=500)
+    
+
+class Shop(User):
+    name = models.CharField(max_length=255)
+    address = models.TextField(max_length=500)
+    phone_one = models.CharField(max_length=14, validators=[phone_number_regex])
+    phone_two = models.CharField(max_length=14, validators=[phone_number_regex],null=True, blank=True)
+
+
+class Customer(User):
+    name = models.CharField(max_length=255)
+    address = models.TextField(max_length=500)
+    phone_one = models.CharField(max_length=14, validators=[phone_number_regex])
+    phone_two = models.CharField(max_length=14, validators=[phone_number_regex],null=True, blank=True)
 
